06307c067f90123fa26f2e4d3b08e8c303050ab6
Wyzzus <wyzzus@gmail.com>
2019-06-29 02:48:49 +0300

    #8 Создал масштабируемую ECS-систему передвижения

---
+++ ShipsUnityProject/Assets/Scripts/MovementSystem/MovableEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Сущность, способная передвигаться
+//Основа для сущности корабля и объектов, которые передвигаются по навмешу
+[RequireComponent(typeof(MotorComponent))]
+public class MovableEntity : Entity
+{
+    // Start is called before the first frame update
+
+    public MotorComponent Motor;
+
+    public override void Start()
+    {
+        base.Start();
+        Motor = (MotorComponent)GetComponentOfType<MotorComponent>();
+    }
+
+    
+}
---
+++ ShipsUnityProject/Assets/Scripts/MovementSystem/ShipEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Сущность корабля
+//Управляет компонентами корабля
+public class ShipEntity : MovableEntity
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void MoveTo(Vector3 point)
+    {
+        Motor.MoveTo(point);
+    }
+}
