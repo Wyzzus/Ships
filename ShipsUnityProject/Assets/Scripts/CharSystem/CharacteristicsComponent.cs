e4cdae937fa0422b476f2a1ee92d166b7af3a5b6
Metramak <boomox.ff@gmail.com>
2019-07-09 00:26:12 +0300

    #35 добавил компонент характеристик корабля

---
+++ ShipsUnityProject/Assets/Scripts/CharSystem/CharacteristicsComponent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacteristicsComponent : EntityComponent
+{
+    
+    [Header ("Info")]
+    public string Name;
+    public int FractionID;
+    public int Level = 0;
+
+    [Header ("HP")]
+    public float BaseHP = 100f;
+    public float CurrentHP;
+    public bool Alive = true;
+
+
+    public override void Start()
+    {
+        base.Start();
+        SetupChars();
+    }
+    public virtual void Update() 
+    {
+        HPCheck();
+    }
+
+    public void SetupChars()
+    {
+        CurrentHP = BaseHP * GetLevelKoeff();
+    }
+
+    public virtual float GetLevelKoeff()
+    {
+        return 1 + (float)Level / 10f;
+    }
+
+    public void HPCheck()
+    {
+        if(CurrentHP > 0) 
+            Alive = true;
+        else 
+            Alive = false;
+    }
+    
+    public int GetDamageLevel()
+    {
+        
+        return 0;
+    }
+
+    public virtual void TakeDamage(float value)
+    {
+        CurrentHP -= value;
+    }
+}
---
+++ ShipsUnityProject/Assets/Scripts/CharSystem/ShipCharComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCharComponent : CharacteristicsComponent
+{
+    [Header("Movement")]
+    public float BaseMoveSpeed = 1f;
+    public float CurrentMoveSpeed;
+    public float BaseTurnSpeed = 1f;
+    public float CurrentTurnSpeed;
+    public bool CanMove = true;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public void SetMoveAbility(bool value)
+    {
+        CanMove = value;
+    }
+    
+    public void SetupMovement()
+    {
+        CurrentMoveSpeed = BaseMoveSpeed * GetLevelKoeff();
+        CurrentTurnSpeed = BaseTurnSpeed * GetLevelKoeff();
+    }
+}
