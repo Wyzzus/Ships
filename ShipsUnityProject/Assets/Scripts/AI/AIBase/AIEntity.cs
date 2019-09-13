31186740d712cf40741c0ba238f8662fa000d416
Wyzzus <wyzzus@gmail.com>
2019-09-13 23:18:28 +0300

    #57 Создал базовые классы ИИ

---
+++ ShipsUnityProject/Assets/Scripts/AI/AIBase/AIEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEntity : Entity
+{
+
+    public override void Start()
+    {
+        Components = new List<EntityComponent>();
+        GetAIComponents();
+    }
+
+    public virtual void GetAIComponents()
+    {
+
+    }
+}
---
+++ ShipsUnityProject/Assets/Scripts/AI/ShipAIEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAIEntity : AIEntity
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
